266bc96e973b49c74efff034af852a46c7450adf
Trenton Bergdahl <trenton.bergdahl@wsu.edu>
2025-02-18 15:03:00 -0800

    Added user configuration from superuser admin view

--- apps/user_management/admin.py
+++ apps/user_management/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser  # Import your custom user model
+from .models import CustomUser
 
-# Register the CustomUser model with the admin site
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'usertype', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'usertype')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'usertype'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- apps/user_management/views.py
+++ apps/user_management/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
-
-
+from .models import CustomUser
 def index(request):
-    return HttpResponse("User Login Page")
+    # Query all users
+    all_users = CustomUser.objects.all()
+
+    # Create a string to hold the user details
+    user_details = "User Login Page<br><br>All Users:<br>"
+
+    # Iterate through the users and add their details to the string
+    for user in all_users:
+        user_details += f"Username: {user.username}, Email: {user.email}, First Name: {user.first_name}, Last Name: {user.last_name}, UserType: {user.usertype}<br>"
+
+    # Return the user details as an HTTP response
+    return HttpResponse(user_details)
