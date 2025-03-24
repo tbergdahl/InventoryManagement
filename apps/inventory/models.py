c738c6852c70924762dc94aa3e062f6184d0f866
Jack Balster <imsojack@gmail.com>
2025-03-24 12:16:21 -0700

    Refactored inventory app and updated item creation

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -5,6 +5,11 @@ class InventoryItem(PolymorphicModel):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     count = models.IntegerField(default=0)
+
+    @property
+    def is_perishable(self):
+        # return true if item has expiry field
+        return hasattr(self, 'expiry_date')
     
 class PerishableInventoryItem(InventoryItem):
     expiry_date = models.DateField()
--- apps/inventory/urls.py
+++ apps/inventory/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path("", views.inventory_main_page, name="inventory_home"),
     path("create/", views.create_inventory_item, name="create_item"),
     path('delete-item/<int:item_id>/', views.delete_item, name='delete_item'),
-    path('create/load', views.load_form, name='load_form'),
 ]
